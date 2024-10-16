686b6c376278f12a71b7f4360e590e20da3dbd7b
liangkaifeng <42540367+liangkaifeng@users.noreply.github.com>
2024-10-16 09:21:46 +0800

    init First release

---
+++ config/electron-builder.js
@@ -0,0 +1,53 @@
+const os = require("os");
+const { notarize } = require("@electron/notarize");
+
+const config = {
+  appId: "asymmetric-key-generator",
+  productName: "AsymmetricKeyGenerator",
+  mac: {
+    target: {
+      target: "dmg",
+      arch: ["universal"],
+    },
+  },
+  win: {
+    target: "nsis",
+  },
+  linux: {
+    target: "AppImage",
+  },
+  afterSign: async (context) => {
+    const { electronPlatformName, appOutDir } = context;
+    if (electronPlatformName !== "darwin") {
+      return;
+    }
+
+    if (
+      !process.env.NOTARIZE_APPLE_ID ||
+      !process.env.NOTARIZE_APPLE_PASSWORD ||
+      !process.env.NOTARIZE_APPLE_TEAM_ID
+    ) {
+      console.log(
+        "Skip notarizing, cannot find NOTARIZE_APPLE_ID or NOTARIZE_APPLE_PASSWORD or NOTARIZE_APPLE_TEAM_ID env"
+      );
+      return;
+    }
+
+    const appName = context.packager.appInfo.productFilename;
+
+    return await notarize({
+      appBundleId: "com.htx.AsymmetricKeyGenerator",
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: process.env.NOTARIZE_APPLE_ID,
+      appleIdPassword: process.env.NOTARIZE_APPLE_PASSWORD,
+      ascProvider: process.env.NOTARIZE_APPLE_TEAM_ID,
+      teamId: process.env.NOTARIZE_APPLE_TEAM_ID,
+    });
+  },
+};
+
+if (os.platform() === "win32" && process.env.WIN_CERTIFICATE_SUBJECT_NAME) {
+  config.win.certificateSubjectName = process.env.WIN_CERTIFICATE_SUBJECT_NAME;
+}
+
+module.exports = config;
